eee0379956e6405c78539ff7eb8bac68ffb8a4af
EXsiDe <petruhin.paulus@yandex.ru>
2025-01-23 04:22:24 +0300

    Create main router

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 import uvicorn
 from fastapi import FastAPI
 
+from api.main_router import api_router
 from core.config import settings
 
 app = FastAPI()
+
+app.include_router(router=api_router, prefix='/api')
 
 if __name__ == '__main__':
     uvicorn.run(
