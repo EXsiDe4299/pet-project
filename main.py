6617ce91bfe44a5c64812a33cf5d44c3f581558f
EXsiDe <petruhin.paulus@yandex.ru>
2025-01-23 09:40:50 +0300

    Add logging middleware for endpoints

--- main.py
+++ main.py
@@ -1,11 +1,23 @@
+import logging
+import time
 from contextlib import asynccontextmanager
 
 import uvicorn
 from fastapi import FastAPI
+from starlette.requests import Request
+from starlette.responses import Response
 
 from api.main_router import api_router
 from core.config import settings
 from core.models.db_helper import db_helper
+
+logging.basicConfig(
+    level=settings.log.log_level_value,
+    format=settings.log.log_format,
+    datefmt=settings.log.date_format
+)
+
+logger = logging.getLogger(__name__)
 
 
 @asynccontextmanager
@@ -17,6 +29,22 @@ async def lifespan(application: FastAPI):
 app = FastAPI(lifespan=lifespan)
 
 app.include_router(router=api_router, prefix='/api')
+
+
+@app.middleware('http')
+async def process_time_log_middleware(request: Request, call_next):
+    start_time = time.time()
+    response: Response = await call_next(request)
+    process_time = round(time.time() - start_time, 3)
+    logger.info(
+        'Method=%s Path=%s StatusCode=%s ProcessTime=%s',
+        request.method,
+        request.url.path,
+        response.status_code,
+        process_time,
+    )
+    return response
+
 
 if __name__ == '__main__':
     uvicorn.run(
