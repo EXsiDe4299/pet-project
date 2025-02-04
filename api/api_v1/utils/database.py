fa767fe8a2c79b2564b525c67447e70cc302b1c2
EXsiDe <petruhin.paulus@yandex.ru>
2025-02-04 21:40:57 +0300

    Create functions for getting and creating user in the database

---
+++ api/api_v1/utils/database.py
@@ -0,0 +1,29 @@
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from core.models import User
+
+
+async def get_user_by_username(
+    username: str,
+    session: AsyncSession,
+) -> User | None:
+    stmt = select(User).where(User.username == username)
+    result = await session.execute(stmt)
+    user = result.scalar_one_or_none()
+    return user
+
+
+async def create_user(
+    username: str,
+    hashed_password: bytes,
+    session: AsyncSession,
+) -> User:
+    new_user = User(
+        username=username,
+        hashed_password=hashed_password,
+    )
+    session.add(new_user)
+    await session.commit()
+    await session.refresh(new_user)
+    return new_user
