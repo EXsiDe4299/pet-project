e147d06cf70394c3fd8bb2f1cd1f744651646622
EXsiDe <petruhin.paulus@yandex.ru>
2025-02-04 21:46:50 +0300

    Create response schemas for auth router

---
+++ api/api_v1/schemas/auth_responses.py
@@ -0,0 +1,25 @@
+from pydantic import BaseModel
+
+from core.config import settings
+
+
+class BaseAuthResponse(BaseModel):
+    status: str = "success"
+
+
+class RegistrationResponse(BaseAuthResponse):
+    pass
+
+
+class LoginResponse(BaseAuthResponse):
+    access_token: str
+    refresh_token: str
+    token_type: str = settings.jwt_auth.token_header_prefix
+
+
+class RefreshResponse(LoginResponse):
+    pass
+
+
+class LogoutResponse(BaseAuthResponse):
+    pass
