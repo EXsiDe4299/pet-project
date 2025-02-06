1e2317a9f54760f703737497a159e8b8085fb073
EXsiDe <petruhin.paulus@yandex.ru>
2025-02-06 12:34:35 +0300

    Create dependencies for jwt auth

---
+++ api/api_v1/dependencies/auth.py
@@ -0,0 +1,62 @@
+import logging
+
+from fastapi import Depends, Cookie
+from fastapi.security import OAuth2PasswordBearer
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from api.api_v1.utils.database import get_user_by_username
+from api.api_v1.utils.jwt_auth import decode_jwt
+from api.api_v1.utils.security import validate_token_type
+from core.config import settings
+from core.models import User
+from core.models.db_helper import db_helper
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/api/v1/auth/login")
+
+logger = logging.getLogger(__name__)
+
+
+async def get_user_from_token(
+    *,
+    token: str,
+    token_type: str,
+    session: AsyncSession,
+) -> User:
+    token_payload = decode_jwt(token=token)
+    if not validate_token_type(token_payload=token_payload, expected_type=token_type):
+        logger.warning("Refreshing failed: invalid token type")
+        raise settings.exc.invalid_token_type_exc
+
+    username = token_payload.get("sub")
+    user = await get_user_by_username(username=username, session=session)
+    if user is None:
+        logger.warning("Refreshing failed: invalid token")
+        raise settings.exc.invalid_token_exc
+
+    if not user.is_active:
+        logger.warning("Refreshing failed: inactive user")
+        raise settings.exc.inactive_user_exc
+
+    return user
+
+
+async def get_current_user_from_access_token(
+    access_token: str = Depends(oauth2_scheme),
+    session: AsyncSession = Depends(db_helper.get_session),
+) -> User:
+    return await get_user_from_token(
+        token=access_token,
+        token_type=settings.jwt_auth.access_token_type,
+        session=session,
+    )
+
+
+async def get_current_user_from_refresh_token(
+    refresh_token: str = Cookie(),
+    session: AsyncSession = Depends(db_helper.get_session),
+) -> User:
+    return await get_user_from_token(
+        token=refresh_token,
+        token_type=settings.jwt_auth.refresh_token_type,
+        session=session,
+    )
