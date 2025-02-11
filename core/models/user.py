1110155eed6025f86970eae778166ed0db19c930
EXsiDe <petruhin.paulus@yandex.ru>
2025-02-12 00:08:31 +0300

    Add email column and columns for email verification to the user model

--- core/models/user.py
+++ core/models/user.py
@@ -10,8 +10,15 @@ class User(Base):
 
     username: Mapped[str] = mapped_column(String(20), primary_key=True)
     hashed_password: Mapped[bytes] = mapped_column(nullable=False)
+    email: Mapped[str] = mapped_column(nullable=False, unique=True)
     is_active: Mapped[bool] = mapped_column(
         nullable=False,
         default=True,
         server_default=expression.true(),
     )
+    is_email_verified: Mapped[bool] = mapped_column(
+        nullable=False,
+        default=False,
+        server_default=expression.false(),
+    )
+    email_verification_token: Mapped[str] = mapped_column(nullable=True)
