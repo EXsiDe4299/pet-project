0d1b5b4568b374b5c9abf62b03c0b2e5c77e8cb0
EXsiDe <petruhin.paulus@yandex.ru>
2025-02-16 02:38:57 +0300

    Change payload data due to changes in the user model

--- api/api_v1/utils/jwt_auth.py
+++ api/api_v1/utils/jwt_auth.py
@@ -55,7 +55,7 @@ def create_jwt(
 
 def create_access_token(user: User):
     access_token_payload = {
-        "sub": user.username,
+        "sub": user.email,
         "username": user.username,
     }
     return create_jwt(
@@ -67,7 +67,7 @@ def create_access_token(user: User):
 
 def create_refresh_token(user: User):
     refresh_token_payload = {
-        "sub": user.username,
+        "sub": user.email,
     }
     return create_jwt(
         token_type=settings.jwt_auth.refresh_token_type,
