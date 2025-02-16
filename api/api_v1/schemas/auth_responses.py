5aaa50874deb7c40cdda22166a3a21ec30a9071f
EXsiDe <petruhin.paulus@yandex.ru>
2025-02-17 01:55:31 +0300

    Create response schemas for forgot and reset password endpoints

--- api/api_v1/schemas/auth_responses.py
+++ api/api_v1/schemas/auth_responses.py
@@ -15,7 +15,7 @@ class ConfirmEmailResponse(BaseAuthResponse):
     pass
 
 
-class ResendingEmailTokenResponse(BaseAuthResponse):
+class SendingEmailTokenResponse(BaseAuthResponse):
     pass
 
 
@@ -23,6 +23,14 @@ class LoginResponse(BaseAuthResponse):
     access_token: str
     refresh_token: str
     token_type: str = settings.jwt_auth.token_header_prefix
+
+
+class ForgotPasswordResponse(BaseAuthResponse):
+    pass
+
+
+class ResetPasswordResponse(BaseAuthResponse):
+    pass
 
 
 class RefreshResponse(LoginResponse):
