775f632d5bacf7cddbae7bcb81a809d26d2c53b5
EXsiDe <petruhin.paulus@yandex.ru>
2025-02-24 09:43:35 +0300

    Fix the echo_pool arg in the db_helper

--- core/models/db_helper.py
+++ core/models/db_helper.py
@@ -44,7 +44,7 @@ class DbHelper:
 db_helper: DbHelper = DbHelper(
     url=str(settings.db.url),
     echo=settings.db.echo,
-    echo_pool=settings.db.echo,
+    echo_pool=settings.db.echo_pool,
     max_overflow=settings.db.max_overflow,
     pool_size=settings.db.pool_size,
 )
