2eefa3d29d14512aded263cf27a9caea5b9db624
EXsiDe <petruhin.paulus@yandex.ru>
2025-03-01 02:28:55 +0300

    Add UserStoryAssociation to __all__

--- core/models/__init__.py
+++ core/models/__init__.py
@@ -3,9 +3,11 @@ __all__ = (
     "User",
     "Token",
     "Story",
+    "UserStoryAssociation",
 )
 
 from core.models.base import Base
 from core.models.user import User
 from core.models.token import Token
 from core.models.story import Story
+from core.models.user_story_association import UserStoryAssociation
