0a00e9a37c703e19176404aa0df987ca524e2e3d
EXsiDe <petruhin.paulus@yandex.ru>
2025-03-08 00:57:34 +0300

    Create BaseStoryScheme. Create StoryInScheme with method as_form

--- api/api_v1/schemas/story.py
+++ api/api_v1/schemas/story.py
@@ -1,13 +1,30 @@
+from fastapi import Form
 from pydantic import BaseModel, UUID4
+
+
+class BasicStoryScheme(BaseModel):
+    name: str
+    text: str
 
 
 class AuthorScheme(BaseModel):
     username: str
 
 
-class StoryScheme(BaseModel):
+class StoryScheme(BasicStoryScheme):
     id: UUID4
     likes_number: int
-    name: str
-    text: str
     author: AuthorScheme
+
+
+class StoryInScheme(BasicStoryScheme):
+    @classmethod
+    def as_form(
+        cls,
+        name: str = Form(),
+        text: str = Form(),
+    ) -> "StoryInScheme":
+        return cls(
+            name=name,
+            text=text,
+        )
