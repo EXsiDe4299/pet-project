bed90db6c13c02264772841f407589f4241ef32e
EXsiDe <petruhin.paulus@yandex.ru>
2025-03-20 20:44:44 +0300

    Use black formatter

--- core/models/token.py
+++ core/models/token.py
@@ -22,8 +22,6 @@ class Token(Base):
         nullable=True
     )
     forgot_password_token: Mapped[str] = mapped_column(nullable=True)
-    forgot_password_token_exp: Mapped[datetime.datetime] = mapped_column(
-        nullable=True
-    )
+    forgot_password_token_exp: Mapped[datetime.datetime] = mapped_column(nullable=True)
 
     __table_args__ = (UniqueConstraint("email"),)
