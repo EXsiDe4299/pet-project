687a2baf2818982bab9dd74e317a27d9ee7abadb
EXsiDe <petruhin.paulus@yandex.ru>
2025-03-20 20:43:33 +0300

    Create validation utilities for files

--- api/api_v1/utils/security.py
+++ api/api_v1/utils/security.py
@@ -1,7 +1,11 @@
 import secrets
 import string
+from io import BytesIO
+from pathlib import Path
 
 import bcrypt
+from PIL import Image
+from fastapi import UploadFile
 
 from core.config import settings
 
@@ -27,3 +31,15 @@ def generate_email_token(length: int = 6) -> str:
     symbols = string.ascii_lowercase + string.digits
     email_verification_token = "".join(secrets.choice(symbols) for _ in range(length))
     return email_verification_token
+
+
+def validate_avatar_extension(avatar: UploadFile) -> bool:
+    avatar_extension = Path(avatar.filename).suffix.lower()
+    return avatar_extension in settings.avatar.allowed_extensions_to_mime.keys()
+
+
+async def validate_avatar_size(avatar: UploadFile) -> bool:
+    content = await avatar.read()
+    image = Image.open(BytesIO(content))
+    await avatar.seek(0)
+    return image.size == settings.avatar.size
