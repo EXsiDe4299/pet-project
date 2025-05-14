298d1760ac5c9636eac1b1fcf61b91a4458d5a60
EXsiDe <petruhin.paulus@yandex.ru>
2025-05-15 02:46:57 +0300

    Fix the association table

--- core/models/user_story_association.py
+++ core/models/user_story_association.py
@@ -1,28 +1,12 @@
-from typing import TYPE_CHECKING
-from uuid import UUID
+from sqlalchemy import ForeignKey, UniqueConstraint, Table, Column
 
-from sqlalchemy import ForeignKey, UniqueConstraint
-from sqlalchemy.orm import Mapped, mapped_column, relationship
-
-from core.models import Base
-
-if TYPE_CHECKING:
-    from core.models import User, Story
+from core.models.base import Base
 
 
-class UserStoryAssociation(Base):
-    __tablename__ = "user_story_association"
-
-    id: Mapped[int] = mapped_column(primary_key=True)
-    user_email: Mapped[str] = mapped_column(ForeignKey("users.email"))
-    story_id: Mapped[UUID] = mapped_column(ForeignKey("stories.id"))
-
-    user: Mapped["User"] = relationship(overlaps="liked_stories,likers")
-    story: Mapped["Story"] = relationship(overlaps="liked_stories,likers")
-
-    __table_args__ = (
-        UniqueConstraint(
-            "user_email",
-            "story_id",
-        ),
-    )
+UserStoryAssociation = Table(
+    "user_story_association",
+    Base.metadata,
+    Column("user_email", ForeignKey("users.email")),
+    Column("story_id", ForeignKey("stories.id")),
+    UniqueConstraint("user_email", "story_id"),
+)
