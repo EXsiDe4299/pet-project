96a4ca71067f00224c3c68fd503f3338f45fd80d
EXsiDe <petruhin.paulus@yandex.ru>
2025-05-24 17:36:05 +0300

    Add column created_at

--- core/models/story.py
+++ core/models/story.py
@@ -1,12 +1,12 @@
+import datetime
 import uuid
 from typing import TYPE_CHECKING
 
-from sqlalchemy import ForeignKey, Text, UUID
+from sqlalchemy import ForeignKey, Text, UUID, func, DateTime
 from sqlalchemy import text as text_
 from sqlalchemy.orm import Mapped, mapped_column, relationship, validates
 
 from core.models.base import Base
-
 from core.models.user_story_association import UserStoryAssociation
 
 if TYPE_CHECKING:
@@ -27,6 +27,12 @@ class Story(Base):
         nullable=False,
         default=0,
         server_default=text_("0"),
+    )
+    created_at: Mapped[datetime.datetime] = mapped_column(
+        DateTime(timezone=True),
+        nullable=False,
+        default=Base.utc_now,
+        server_default=func.now(),
     )
 
     author_email: Mapped[str] = mapped_column(ForeignKey("users.email"), nullable=False)
