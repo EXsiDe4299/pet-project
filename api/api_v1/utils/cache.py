67c6ca147e3062a68053231204614ebcfa49d2cb
EXsiDe <petruhin.paulus@yandex.ru>
2026-03-02 16:22:48 +0300

    Add logging to the add_token_to_blacklist function

--- api/api_v1/utils/cache.py
+++ api/api_v1/utils/cache.py
@@ -1,16 +1,37 @@
+from api.api_v1.dependencies.log_helper import LogHelper
 from api.api_v1.exceptions.http_exceptions import InvalidJWT
 from redis.asyncio import Redis
+
+logger = LogHelper.get_app_logger()
 
 
 async def add_token_to_blacklist(
     payload: dict,
     cache: Redis,
 ) -> None:
+    sub = payload.get("sub")
     jti = payload.get("jti")
     exp = payload.get("exp")
+
+    logger.debug(
+        "Adding JWT to blacklist. Sub=%r, JTI=%r",
+        sub,
+        jti,
+    )
+
     if jti is None or exp is None:
+        logger.warning(
+            "Adding JWT to blacklist failed. Token missing JTI or EXP. Sub=%r, Payload keys=%s",
+            sub,
+            payload.keys(),
+        )
         raise InvalidJWT()
     await cache.set(name=jti, value="", exat=exp)
+    logger.debug(
+        "JWT added to blacklist successfully. Sub=%r, JTI=%r",
+        sub,
+        jti,
+    )
 
 
 async def is_token_in_blacklist(jti: str, cache: Redis) -> bool:
